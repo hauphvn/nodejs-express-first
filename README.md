094d5bc47e96b9f31eed3cfad3438e8629232f92
Hau <hau.pham@gigacover.com>
2019-09-28 15:00:37 +0700

    install database fake to test, build creating a new member, quering

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
     - ``$ npm init``
     - ``$ npm install express``
     - ``$ npm install -g nodemon`` to code faster
+    - ``$ npm install pug`` to convert pug code to html code
 2. Coding
     - Create a index.js file.
--- index.js
+++ index.js
@@ -2,12 +2,64 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//lowdb
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: []})
+    .write();
+
+var bodyParser = require('body-parser');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/',function (req, res) {
-    res.send("<h1>Hello guy's</h1> <p>Click <a href='/user'>here</a> to user page</p>");
+    res.render('index',{
+        name:'aaa'
+    });
 });
 
-app.get('/user',function (req, res) {
-    res.send("<h2>List user</h2>");
+app.get('/users',function (req, res) {
+    res.render('users/index',{
+        users:db.get('users').value()
+    });
+});
+
+app.get('/users/search', function (req, res) {
+    let q = req.query.name;
+    let matchedUser = db.get('users').value().filter(function (item) {
+        return item.name.toLowerCase().indexOf((q.toLowerCase())) !== -1;
+    });
+    res.render('users/index',{
+        users:matchedUser
+    });
+
+});
+
+app.post('/users/create',function (req, res) {
+    let theName = req.body.name;
+    let thePhone = req.body.phone;
+    let user = {
+        // id: users.length + 1,
+        name: theName,
+        phone: thePhone
+    };
+    db.get('users').push(user).write();
+    res.redirect('/users');
+});
+
+app.get('/users/create',function (req, res) {
+    res.render('users/create');
 });
 
 app.listen(port, function(){
